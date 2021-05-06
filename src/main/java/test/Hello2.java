e98e02d10ea2cc734bfe93791f8cced3f96df09b
EladSalti <elad.salti7@gmail.com>
2021-05-06 15:14:55 +0300

    first commit

---
+++ src/main/java/test/Hello2.java
@@ -0,0 +1,11 @@
+package test;
+
+import io.netty.handler.ssl.OpenSslEngine;
+
+
+public abstract class Hello2{
+
+    public void eat(){
+        OpenSslEngine openSslEngine = new OpenSslEngine(2, null, null);
+    }
+}
